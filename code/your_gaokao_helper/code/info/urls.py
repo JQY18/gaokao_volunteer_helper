a643309ba5e99bd3922b6a5f0e893f44b9ba73f2
JQY18 <3165973561@qq.com>
2024-11-07 16:04:09 +0800

    ...

--- code/your_gaokao_helper/code/info/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views3
-from . import views
-
-urlpatterns = [
-    path('api/info/', views.get_data),
-    path('info/', views3.info, name='info'),
-    path('analy', views3.analy, name='analy')
-]
